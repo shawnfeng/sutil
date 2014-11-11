d6d778a7ecacfa2de170eb1902a3ab68ec3e9597
Shawn Feng <fengguangxiang@gmail.com>
2014-11-11 16:16:01 +0800

    add easy config

---
+++ sconf/A_EasyConf_test.go
@@ -0,0 +1,127 @@
+package sconf
+
+import (
+	"testing"
+
+	"fmt"
+)
+
+
+func TestToInt(t *testing.T) {
+	ef := NewTierConf()
+
+	c := make(map[string]map[string]string)
+
+	pv := map[string]string {
+		"p0": "v0",
+		"p1": "v1",
+		"p2": "v2",
+		"p3": "1234",
+
+	}
+
+	pv2 := map[string]string {
+
+		"p4": "a,  bc,\t   de   ",
+		"p5": "ada;afew;  qwe",
+
+		"p6": "1;2;q;3",
+		"p7": "-1;2000;134123;3",
+	}
+
+	c["s0"] = pv
+	ef.LoadFromConf(c)
+	c["s1"] = pv
+	ef.LoadFromConf(c)
+	c["s0"] = pv2
+	c["s1"] = pv2
+	c["s2"] = pv2
+	ef.LoadFromConf(c)
+
+	v, err := ef.ToInt("nsection", "aa")
+	fmt.Println(v, err)
+	if err.Error() != "section empty" {
+		t.Errorf("section")
+	}
+
+
+	v, err = ef.ToInt("s0", "npro")
+	fmt.Println(v, err)
+	if err.Error() != "property empty" {
+		t.Errorf("property")
+	}
+
+
+
+	v, err = ef.ToInt("s0", "p0")
+	fmt.Println(v, err)
+	if err == nil {
+		t.Errorf("convert")
+	}
+
+
+	v, err = ef.ToInt("s1", "p3")
+	fmt.Println(v, err)
+	if err != nil {
+		t.Errorf("err")
+	}
+
+
+	v = ef.ToIntWithDefault("s0", "npro", 33)
+	fmt.Println(v)
+
+
+	s, err := ef.ToString("nsection", "aa")
+	fmt.Println(s, err)
+	if err.Error() != "section empty" {
+		t.Errorf("section")
+	}
+
+
+	s, err = ef.ToString("s2", "p2")
+	fmt.Println(s, err)
+
+
+	s = ef.ToStringWithDefault("nsection", "aa", "def")
+	fmt.Println(s)
+
+
+
+	ss, err := ef.ToSliceString("nsecion", "aa", ",")
+	fmt.Println(ss, err)
+	if err.Error() != "section empty" {
+		t.Errorf("section")
+	}
+
+
+
+	ss, err = ef.ToSliceString("s0", "p4", ",")
+	fmt.Println(ss, err)
+
+	ss, err = ef.ToSliceString("s0", "p4", ";")
+	fmt.Println(ss, err)
+
+
+	ss, err = ef.ToSliceString("s0", "p5", ",")
+	fmt.Println(ss, err)
+
+
+	ss, err = ef.ToSliceString("s0", "p5", ";")
+	fmt.Println(ss, err)
+
+	is, err := ef.ToSliceInt("nsecion", "aa", ",")
+	fmt.Println(is, err)
+	if err.Error() != "section empty" {
+		t.Errorf("section")
+	}
+
+
+	is, err = ef.ToSliceInt("s1", "p6", ";")
+	fmt.Println(is, err)
+
+
+	is, err = ef.ToSliceInt("s1", "p7", ";")
+	fmt.Println(is, err)
+
+
+}
--- sconf/TierConf.go
+++ sconf/TierConf.go
@@ -2,25 +2,42 @@ package sconf
 
 import (
 	"bytes"
+	"errors"
+	"strconv"
+	"strings"
 	"github.com/vaughan0/go-ini"
 )
 
 
 type TierConf struct {
-	res map[string]map[string]string
+	conf map[string]map[string]string
 
 }
 
 func NewTierConf() *TierConf {
 
 	return &TierConf{
-		res: make(map[string]map[string]string),
+		conf: make(map[string]map[string]string),
 
 	}
 }
 
 func (m *TierConf) GetConf() map[string]map[string]string {
-	return m.res
+	return m.conf
+}
+
+func (m *TierConf) LoadFromConf(cfg map[string]map[string]string) {
+	for name, section := range cfg {
+		if _, ok := m.conf[name]; !ok {
+			m.conf[name] = make(map[string]string)
+		}
+
+		for k, v := range section {
+			m.conf[name][k] = v
+		}
+	}
+
+
 }
 
 func (m *TierConf) Load(cfg []byte) error {
@@ -31,12 +48,12 @@ func (m *TierConf) Load(cfg []byte) error {
 	}
 
 	for name, section := range file {
-		if _, ok := m.res[name]; !ok {
-			m.res[name] = make(map[string]string)
+		if _, ok := m.conf[name]; !ok {
+			m.conf[name] = make(map[string]string)
 		}
 
 		for k, v := range section {
-			m.res[name][k] = v
+			m.conf[name][k] = v
 		}
 	}
 
@@ -45,3 +62,101 @@ func (m *TierConf) Load(cfg []byte) error {
 
 }
 
+
+func (m *TierConf) ToSection(section string) (map[string]string, error) {
+	if s, ok := m.conf[section]; ok {
+		return s, nil
+	} else {
+		return nil, errors.New("section empty")
+	}
+
+
+}
+
+func (m *TierConf) ToString(section string, property string) (string, error) {
+	s, err := m.ToSection(section)
+
+	if err != nil {
+		return "", err
+
+	} else {
+		if p, ok := s[property]; ok {
+			return p, nil
+		} else {
+			return "", errors.New("property empty")
+		}
+
+
+	}
+
+}
+
+func (m *TierConf) ToStringWithDefault(section string, property string, deft string) string {
+	v, err := m.ToString(section, property)
+
+	if err != nil {
+		return deft
+	} else {
+		return v
+	}
+
+
+}
+
+func (m *TierConf) ToInt(section string, property string) (int, error) {
+	v, err := m.ToString(section, property)
+
+	if err != nil {
+		return 0, err
+	} else {
+		return strconv.Atoi(v)
+	}
+
+}
+
+func (m *TierConf) ToIntWithDefault(section string, property string, deft int) int {
+	v, err := m.ToInt(section, property)
+	if err != nil {
+		return deft
+
+	} else {
+		return v
+	}
+}
+
+func (m *TierConf) ToSliceString(section string, property string, sep string) ([]string, error) {
+	v, err := m.ToString(section, property)
+
+	if err != nil {
+		return nil, err
+	} else {
+		ss := strings.Split(v, sep)
+
+		for i := 0; i < len(ss); i++ {
+			ss[i] = strings.Trim(ss[i], " \t")
+		}
+		return ss, nil
+	}
+
+}
+
+func (m *TierConf) ToSliceInt(section string, property string, sep string) ([]int, error) {
+	s, err := m.ToSliceString(section, property, sep)
+	if err != nil {
+		return nil, err
+	} else {
+		ints := make([]int, 0)
+		for _, v := range(s) {
+			tmp, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, err
+			} else {
+				ints = append(ints, tmp)
+			}
+
+		}
+
+		return ints, nil
+	}
+
+}
