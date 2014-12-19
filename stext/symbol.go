545e8b460a88cca450cdb280e5e6c98136de5722
Shawn Feng <fengguangxiang@gmail.com>
2014-12-19 20:35:33 +0800

    stext util

---
+++ stext/symbol.go
@@ -0,0 +1,116 @@
+package stext
+
+import (
+	"fmt"
+	"io/ioutil"
+	"bytes"
+
+	"unicode/utf8"
+)
+
+
+type SymbolList struct {
+	list map[rune] []byte
+}
+
+func NewSymbolList(file string) (*SymbolList, error) {
+
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	m := &SymbolList {
+		list: make(map[rune] []byte),
+	}
+
+	items := bytes.Split(data, []byte("\n"))
+	for _, it := range(items) {
+		r, size := utf8.DecodeRune(it)
+		if size == len(it) {
+			_, ok := m.list[r]
+			if ok {
+				//slog.Warnf("same %c %d %s %v", r, size, it, it)
+			} else {
+				m.list[r] = it
+			}
+		} else {
+			//slog.Warnf("illigal %s %d %s %v", r, size, it, it)
+		}
+	}
+
+	for i := 0; i < 256; i++ {
+		if i >= 'A' && i <= 'Z' {
+			continue
+		}
+
+		if i >= 'a' && i <= 'z' {
+			continue
+		}
+
+		if i >= '0' && i <= '9' {
+			continue
+		}
+
+		m.list[rune(i)] = []byte{byte(i)}
+
+	}
+
+
+	return m, nil
+}
+
+func (m *SymbolList)Is(c rune) bool {
+	_, ok := m.list[c]
+	return ok
+}
+
+
+func (m *SymbolList) Print() {
+
+	s := ""
+	for _, v := range(m.list) {
+		if len(s) == 0 {
+			s = fmt.Sprintf("%s", v)
+		} else {
+			s = fmt.Sprintf("%s,%s", s, v)
+		}
+	}
+
+	fmt.Println(s)
+}
+
+
+func BytesToRunesNoSymb(s *SymbolList, b []byte) []rune {
+	rv := make([]rune, 0)
+	for len(b) > 0 {
+		r, size := utf8.DecodeRune(b)
+
+		b = b[size:]
+
+		if s.Is(r) {
+			continue
+		}
+
+
+		rv = append(rv, r)
+		
+	}
+
+	return rv
+}
+
+
+
+func BytesToRunes(b []byte) []rune {
+	rv := make([]rune, 0)
+	for len(b) > 0 {
+		r, size := utf8.DecodeRune(b)
+		rv = append(rv, r)
+		b = b[size:]
+		rv = append(rv, r)
+
+	}
+
+	return rv
+}
---
+++ stext/symbol_test.go
@@ -0,0 +1,59 @@
+package stext
+
+import (
+	"testing"
+	"unicode/utf8"
+	"github.com/shawnfeng/sutil/slog"
+)
+
+
+func TestSymb(t *testing.T) {
+	s, err := NewSymbolList("symbol.list")
+	if err != nil {
+		slog.Errorln("load symbol")
+	}
+
+
+	b := []byte("\t!@#$%^&*不同国家的女人难受时如何用同一句话安慰？神回复：欧美 - You need cry，dear（你需要哭出来，宝贝）中国 - 有你\n的快递儿！")
+	bb := []byte("\t!@#$%^&*不同国家的女人难受时如何用同一句话安慰？神回复：欧美 - You need cry，dear（你需要哭出来，宝贝）中国 - 有你\n的快递儿！")
+
+
+	cmp := "\t!@#$%^&*？： -   ，（，） - \n！"
+	pick := ""
+	for len(b) > 0 {
+		r, size := utf8.DecodeRune(b)
+		if s.Is(r) {
+			slog.Infof("%c", r)
+			pick += string(b[:size])
+		}
+		b = b[size:]
+	}
+
+	slog.Infoln("cmp:", cmp)
+	slog.Infoln("pick:", pick)
+
+	if cmp != pick {
+		t.Errorf("not ok")
+	}
+
+	rv := BytesToRunesNoSymb(s, bb)
+
+	slog.Infof("%s   %s", bb, rv)
+
+	for i := 0; i < len(rv); i++ {
+		slog.Infof("%c", rv[i])
+	}
+
+
+
+	rv = BytesToRunes(bb)
+
+	slog.Infof("%s   %s", bb, rv)
+
+	for i := 0; i < len(rv); i++ {
+		slog.Infof("%c", rv[i])
+	}
+
+	//s.Print()
+
+}
