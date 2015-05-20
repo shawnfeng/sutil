79dd9120211a1256a9c84601e2823d8de1496d8b
Shawn Feng <fengguangxiang@gmail.com>
2015-05-20 11:32:01 +0800

    add head

--- semail/send.go
+++ semail/send.go
@@ -1,3 +1,8 @@
+// Copyright 2014 The sutil Author. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+
 package semail
  
 import (
