8e75dd8c4f85bace9c8d7e96fcfce3cf0626a499
zhenghe3119 <zhenghe3119@ipalfish.com>
2019-05-28 16:30:00 +0800

    add trace module for config and initialization

---
+++ trace/tracer.go
@@ -0,0 +1,59 @@
+package trace
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/shawnfeng/sutil/slog"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+)
+
+// singleton
+var bt *backTracer
+
+func InitDefaultTracer(serviceName string) error {
+	return InitTracer(TRACER_TYPE_JAEGER, serviceName)
+}
+
+func InitTracer(tracerType string, serviceName string) error {
+	// only init once
+	if bt != nil {
+		return nil
+	}
+	switch tracerType {
+	case TRACER_TYPE_JAEGER:
+		return initJaeger(serviceName)
+	default:
+		return fmt.Errorf("unsupported tracer type:%d", tracerType)
+	}
+}
+
+func CloseTracer() error {
+	if bt != nil {
+		return bt.closer.Close()
+	}
+	return nil
+}
+
+type backTracer struct {
+	tracer opentracing.Tracer
+	closer io.Closer
+}
+
+func initJaeger(serviceName string) error {
+	fun := "initJaeger-->"
+	defaultConfig := defaultConfigurator.GetConfig(serviceName)
+	if cfg, ok := defaultConfig.TracerConfig.(config.Configuration); !ok {
+		return fmt.Errorf("wrong tracer config:%v", cfg)
+	} else {
+		tracer, closer, err := cfg.NewTracer()
+		if err != nil {
+			return err
+		}
+		bt = &backTracer{tracer, closer}
+
+		opentracing.SetGlobalTracer(bt.tracer)
+		slog.Infof("%s succeed for %s", fun, serviceName)
+		return nil
+	}
+}
