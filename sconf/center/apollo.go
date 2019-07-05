03aaa8d03225bc0138c24d051f987e3868a211c8
zhenghe3119 <zhenghe3119@ipalfish.com>
2019-07-05 14:50:59 +0800

    move trace out to interface.go

--- sconf/center/apollo.go
+++ sconf/center/apollo.go
@@ -3,7 +3,6 @@ package center
 import (
 	"context"
 	"github.com/ZhengHe-MD/agollo"
-	"github.com/opentracing/opentracing-go"
 	"github.com/shawnfeng/sutil/slog/slog"
 	"os"
 	"sync"
@@ -54,9 +53,6 @@ func confFromEnv() *agollo.Conf {
 }
 
 func (ap *apolloConfigCenter) Init(ctx context.Context, serviceName string, namespaceNames []string) error {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.Init")
-	defer span.Finish()
-
 	conf := confFromEnv()
 	conf.AppID = serviceName
 
@@ -71,59 +67,35 @@ func (ap *apolloConfigCenter) Init(ctx context.Context, serviceName string, name
 }
 
 func (ap *apolloConfigCenter) Stop(ctx context.Context) error {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.Stop")
-	defer span.Finish()
-
 	return agollo.Stop()
 }
 
 func (ap *apolloConfigCenter) GetString(ctx context.Context, key string) string {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetString")
-	defer span.Finish()
-
 	return agollo.GetString(key, "")
 }
 
 func (ap *apolloConfigCenter) GetStringWithNamespace(ctx context.Context, namespace, key string) string {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetStringWithNamespace")
-	defer span.Finish()
-
 	return agollo.GetStringWithNamespace(namespace, key, "")
 }
 
 func (ap *apolloConfigCenter) GetBool(ctx context.Context, key string) bool {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetBool")
-	defer span.Finish()
-
 	return agollo.GetBool(key, false)
 }
 
 func (ap *apolloConfigCenter) GetBoolWithNamespace(ctx context.Context, namespace, key string) bool {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetBoolWithNamespace")
-	defer span.Finish()
-
 	return agollo.GetBoolWithNamespace(namespace, key, false)
 }
 
 func (ap *apolloConfigCenter) GetInt(ctx context.Context, key string) int {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetInt")
-	defer span.Finish()
-
 	return agollo.GetInt(key, 0)
 }
 
 func (ap *apolloConfigCenter) GetIntWithNamespace(ctx context.Context, namespace, key string) int {
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetIntWithNamespace")
-	defer span.Finish()
-
 	return agollo.GetIntWithNamespace(namespace, key, 0)
 }
 
 func (ap *apolloConfigCenter) WatchUpdate(ctx context.Context) <-chan *ChangeEvent {
 	fun := "sconfcenter.WatchUpdate-->"
-
-	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.WatchUpdate")
-	defer span.Finish()
 
 	ap.watchUpdateOnce.Do(func() {
 		agolloChangeEventChan := agollo.WatchUpdate()
--- sconf/center/interface.go
+++ sconf/center/interface.go
@@ -1,6 +1,9 @@
 package center
 
-import "context"
+import (
+	"context"
+	"github.com/opentracing/opentracing-go"
+)
 
 var defaultConfigCenter ConfigCenter
 
@@ -19,38 +22,65 @@ type ConfigCenter interface {
 }
 
 func Init(ctx context.Context, serviceName string, namespaceNames []string) error {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.Init")
+	defer span.Finish()
+
 	defaultConfigCenter = newApolloConfigCenter()
 	return defaultConfigCenter.Init(ctx, serviceName, namespaceNames)
 }
 
 func Stop(ctx context.Context) error {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.Stop")
+	defer span.Finish()
+
 	return defaultConfigCenter.Stop(ctx)
 }
 
 func GetString(ctx context.Context, key string) string {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetString")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetString(ctx, key)
 }
 
 func GetStringWithNamespace(ctx context.Context, namespace, key string) string {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetStringWithNamespace")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetStringWithNamespace(ctx, namespace, key)
 }
 
 func GetBool(ctx context.Context, key string) bool {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetBool")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetBool(ctx, key)
 }
 
 func GetBoolWithNamespace(ctx context.Context, namespace, key string) bool {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetBoolWithNamespace")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetBoolWithNamespace(ctx, namespace, key)
 }
 
 func GetInt(ctx context.Context, key string) int {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetInt")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetInt(ctx, key)
 }
 
 func GetIntWithNamespace(ctx context.Context, namespace, key string) int {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.GetIntWithNamespace")
+	defer span.Finish()
+
 	return defaultConfigCenter.GetIntWithNamespace(ctx, namespace, key)
 }
 
 func WatchUpdate(ctx context.Context) <-chan *ChangeEvent {
+	span, _ := opentracing.StartSpanFromContext(ctx, "sconfcenter.WatchUpdate")
+	defer span.Finish()
+
 	return defaultConfigCenter.WatchUpdate(ctx)
 }
