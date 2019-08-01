fb8d27adbeca9944090ec52089527e35f89a7723
tanghuaicheng <tanghuaicheng2015@ipalfish.com>
2019-08-01 20:36:38 +0800

    add pprof router

--- smetric/metricsprocessor.go
+++ smetric/metricsprocessor.go
@@ -4,6 +4,7 @@ import (
 	"github.com/julienschmidt/httprouter"
 	"github.com/shawnfeng/sutil/slog"
 	"net/http"
+	"net/http/pprof"
 )
 
 var (
@@ -27,6 +28,28 @@ func (p *Metricsprocessor) Driver() (string, interface{}) {
 	handlerFor := p.Metrics.Exportor()
 	router := httprouter.New()
 	router.Handler("GET", default_metric_location, handlerFor)
+	router.Handler("GET", "/debug/pprof/", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/allocs", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/allocs", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/block", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/block", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/goroutine", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/goroutine", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/heap", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/heap", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/mutex", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/mutex", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/threadcreate", &routeHandlerAdapter{pprof.Index})
+	router.Handler("POST", "/debug/pprof/threadcreate", &routeHandlerAdapter{pprof.Index})
+	router.Handler("GET", "/debug/pprof/cmdline", &routeHandlerAdapter{pprof.Cmdline})
+	router.Handler("POST", "/debug/pprof/cmdline", &routeHandlerAdapter{pprof.Cmdline})
+	router.Handler("GET", "/debug/pprof/profile", &routeHandlerAdapter{pprof.Profile})
+	router.Handler("POST", "/debug/pprof/profile", &routeHandlerAdapter{pprof.Profile})
+	router.Handler("GET", "/debug/pprof/trace", &routeHandlerAdapter{pprof.Trace})
+	router.Handler("POST", "/debug/pprof/trace", &routeHandlerAdapter{pprof.Trace})
+	router.Handler("GET", "/debug/pprof/symbol", &routeHandlerAdapter{pprof.Symbol})
+	router.Handler("POST", "/debug/pprof/symbol", &routeHandlerAdapter{pprof.Symbol})
 	//router.Handler("POST", p.location,handlerFor)
 	router.HandlerFunc("GET", "/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(`<html>
@@ -39,6 +62,15 @@ func (p *Metricsprocessor) Driver() (string, interface{}) {
 	})
 	return p.getListenerAddress(), router
 }
+
+type routeHandlerAdapter struct {
+	ProfFunc func(w http.ResponseWriter, r *http.Request)
+}
+
+func (m *routeHandlerAdapter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	m.ProfFunc(w, r)
+}
+
 func (p *Metricsprocessor) getListenerAddress() string {
 	return ""
 	//port := 22333
