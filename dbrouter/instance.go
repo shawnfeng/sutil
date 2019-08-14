03593cbc44a837fc4d1c7a42b3f7b2be0b337c79
wangle3538 <wangle3538@ipalfish.com>
2019-08-14 15:56:27 +0800

    Fix thread-safe problem of db instance

--- dbrouter/instance.go
+++ dbrouter/instance.go
@@ -6,6 +6,7 @@ package dbrouter
 
 import (
 	"context"
+	"fmt"
 	"github.com/shawnfeng/sutil/scontext"
 	"github.com/shawnfeng/sutil/slog/slog"
 	"sync"
@@ -21,15 +22,15 @@ type Instancer interface {
 type FactoryFunc func(ctx context.Context, key, group string) (in Instancer, err error)
 
 type InstanceManager struct {
-	instances map[string]*sync.Map
-	factory   FactoryFunc
-	groups    []string
+	instanceMu sync.Mutex
+	instances  sync.Map
+	factory    FactoryFunc
+	groups     []string
 }
 
 func NewInstanceManager(factory FactoryFunc, dbChangeChan chan dbConfigChange, groups []string) *InstanceManager {
 	instanceManager := &InstanceManager{
 		factory: factory,
-		instances: make(map[string]*sync.Map),
 		groups: groups,
 	}
 
@@ -38,43 +39,31 @@ func NewInstanceManager(factory FactoryFunc, dbChangeChan chan dbConfigChange, g
 	return instanceManager
 }
 
-func (m *InstanceManager) Get(ctx context.Context, key string) Instancer {
-	fun := "InstanceManager.get -->"
+func (m *InstanceManager) buildKey(instance, group string) string {
+	return fmt.Sprintf("%s-%s", group, instance)
+}
+
+func (m *InstanceManager) Get(ctx context.Context, instance string) Instancer {
+	fun := "InstanceManager.Get -->"
 
 	var err error
 	var in interface{}
-	var instanceMap *sync.Map
 	group := scontext.GetGroup(ctx)
 
-	isConfig := false
-	for _, configGroup := range m.groups {
-		if group == configGroup {
-			isConfig = true
-		}
-	}
-
-	if !isConfig {
+	if !m.isInGroup(group) {
 		group = DefaultGroup
 	}
 
-	if ins, ok := m.instances[group]; ok {
-		instanceMap = ins
-	} else {
-		m.instances[group] = new(sync.Map)
-		instanceMap = m.instances[group]
-	}
-
-	in, ok := instanceMap.Load(key)
+	key := m.buildKey(instance, group)
+	in, ok := m.instances.Load(key)
 	if ok == false {
-
-		slog.Infof(ctx, "%s newInstance, key: %s", fun, key)
-		in, err = m.factory(ctx, key, group)
+		slog.Infof(ctx, "%s newInstance, instance: %s", fun, instance)
+		in, err = m.buildInstance(ctx, instance, group)
 		if err != nil {
-			slog.Errorf(ctx, "%s NewInstance err, key: %s, err: %s", fun, key, err.Error())
+			slog.Errorf(ctx, "%s NewInstance err, instance: %s, err: %s", fun, instance, err.Error())
 			return nil
 		}
 
-		in, _ = instanceMap.LoadOrStore(key, in)
 	}
 
 	tmp, ok := in.(Instancer)
@@ -86,76 +75,85 @@ func (m *InstanceManager) Get(ctx context.Context, key string) Instancer {
 	return tmp
 }
 
+func (m *InstanceManager) isInGroup(group string) bool {
+	for _, configGroup := range m.groups {
+		if group == configGroup {
+			return true
+		}
+	}
+	return false
+}
+
+func (m *InstanceManager) buildInstance(ctx context.Context, instance, group string) (interface{}, error) {
+	m.instanceMu.Lock()
+	defer m.instanceMu.Unlock()
+	if group != DefaultGroup {
+		if !m.isInGroup(group) {
+			group = DefaultGroup
+		}
+	}
+	tmp, err := m.factory(ctx, instance, group)
+	if err != nil {
+		return nil, err
+	}
+
+	key := m.buildKey(instance, group)
+	in, _ := m.instances.LoadOrStore(key, tmp)
+
+	return in, nil
+}
+
 func (m *InstanceManager) Close() {
 	fun := "InstanceManager.Close -->"
 
-	for group, ins := range m.instances {
-		ins.Range(func(key, value interface{}) bool {
-			slog.Infof(context.TODO(), "%s key:%v", fun, key)
+	m.instances.Range(func(key, value interface{}) bool {
+		slog.Infof(context.TODO(), "%s key:%v", fun, key)
 
-			in, ok := value.(Instancer)
-			if ok == false {
-				slog.Errorf(context.TODO(), "%s value.(Instancer) false, key: %v", fun, key)
-				return false
-			}
+		in, ok := value.(Instancer)
+		if ok == false {
+			slog.Errorf(context.TODO(), "%s value.(Instancer) false, key: %v", fun, key)
+			return false
+		}
 
-			in.Close()
-			ins.Delete(key)
+		in.Close()
+		m.instances.Delete(key)
 
-			return true
-		})
-
-		delete(m.instances, group)
-	}
+		return true
+	})
 }
 
 func (m *InstanceManager) dbInsChangeHandler(ctx context.Context, dbChangeChan chan dbConfigChange) {
 	fun := "InstanceManager.dbInsChangeHandler -->"
-	var originGroups []string
 	for dbInsChange := range dbChangeChan {
 		slog.Infof(ctx, "%s receive db instance changes: %+v", fun, dbInsChange)
-		originGroups = m.groups
 		m.groups = dbInsChange.dbGroups
-		for group, insNames := range dbInsChange.dbInstanceChange {
-			for _, insName := range insNames {
-				m.closeDbInstance(ctx, insName, group)
-			}
-		}
-
-		for _, group := range originGroups {
-			if !isInStringList(group, m.groups) {
-				delete(m.instances, group)
-				slog.Infof(ctx, "%s succeed delete group %s", fun, group)
-			}
-		}
+		m.handleDbInsChange(ctx, dbInsChange.dbInstanceChange)
 	}
 }
 
-func isInStringList(item string, strList []string) bool {
-	for _, str := range strList {
-		if str == item {
-			return true
+func (m *InstanceManager) handleDbInsChange(ctx context.Context, dbInstanceChange map[string][]string) {
+	m.instanceMu.Lock()
+	defer m.instanceMu.Unlock()
+	for group, insNames := range dbInstanceChange {
+		for _, insName := range insNames {
+			m.closeDbInstance(ctx, insName, group)
 		}
 	}
-
-	return false
 }
 
 func (m *InstanceManager) closeDbInstance(ctx context.Context, insName, group string) {
 	fun := "InstanceManager.closeDbInstance -->"
-	if _, ok := m.instances[group]; !ok {
-		return
-	}
-	if ins, ok := m.instances[group].Load(insName); ok {
-		m.instances[group].Delete(insName)
+	key := m.buildKey(insName, group)
+	if ins, ok := m.instances.Load(key); ok {
+		m.instances.Delete(key)
 		if in, ok := ins.(Instancer); ok {
 			if err := in.Close(); err == nil {
-				slog.Infof(ctx, "%s succeed to close db instance %s", fun, insName)
+				slog.Infof(ctx, "%s succeed to close db instance: %s group: %s", fun, insName, group)
 			} else {
-				slog.Warnf(ctx, "%s close db instance %s error: %s", fun, insName, err.Error())
+				slog.Warnf(ctx, "%s close db instance: %s group: %s error: %s", fun, insName, group, err.Error())
 			}
 		} else {
-			slog.Warnf(ctx, "%s close db instance %s error, not Instancer type", fun, insName)
+			slog.Warnf(ctx, "%s close db instance: %s group: %s error, not Instancer type", fun, insName, group)
 		}
 	}
 }
