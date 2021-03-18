ab7ac295dcc30388cae346f51c4238f3973901f2
xyzzhang <1490828193@qq.com>
2021-03-18 11:33:43 +0800

    smetric/sla.go: CollectServV2

--- smetric/sla.go
+++ smetric/sla.go
@@ -16,6 +16,7 @@ const (
 	Label_type                  = "type"
 	Label_source                = "source"
 	Label_status                = "status"
+	Label_errcode               = "errcode"
 	Status_succ                 = 1
 	Status_fail                 = 0
 )
@@ -54,6 +55,45 @@ func buildSerLabels(instance, servkey string, servid int, processor string, sour
 }
 func buildSerReqLabels(instance, servkey string, servid int, processor string, source int, funcName string, status int) []Label {
 	labels := buildSerLabels(instance, servkey, servid, processor, source, funcName)
+	labels = append(labels, Label{
+		Name: Label_status, Value: strconv.Itoa(status),
+	})
+	return labels
+}
+
+//服务相关数据收集，主要包括：成功率、响应时间
+//instance:当前服务实例
+//servkey:服务标识如base/sla；processor：进程类型 proc_thrift/proc_http
+//duration耗时；source请求源标识，目前都是0；servid：服务id标识；funcName：监控的api标识
+//err 是否处理有报错
+//增加errCode: 自定义错误码
+func CollectServV2(instance, servkey string, servid int, processor string, duration time.Duration, source int, funcName string, errCode int, err interface{}) {
+	durlabels := buildSerLabelsV2(instance, servkey, servid, processor, source, funcName, errCode)
+	DefaultMetrics.AddHistoramSampleCreateIfAbsent(metricDurationNameKeys, duration.Seconds(), durlabels, nil)
+	var counterLabels []Label
+	if err == nil {
+		counterLabels = buildSerReqLabelsV2(instance, servkey, servid, processor, source, funcName, errCode, Status_succ)
+	} else {
+		counterLabels = buildSerReqLabelsV2(instance, servkey, servid, processor, source, funcName, errCode, Status_fail)
+	}
+	DefaultMetrics.IncrCounterCreateIfAbsent(metricReqNameKeys, 1.0, counterLabels)
+}
+func buildSerLabelsV2(instance, servkey string, servid int, processor string, source int, funcName string, errCode int) []Label {
+	targetServerName := SafePromethuesValue(servkey)
+	instance = SafePromethuesValue(instance)
+	sid := strconv.Itoa(servid)
+	return []Label{
+		{Name: Label_instance, Value: instance},
+		{Name: Label_servname, Value: targetServerName},
+		{Name: Label_servid, Value: sid},
+		{Name: Label_api, Value: funcName},
+		{Name: Label_source, Value: strconv.Itoa(source)},
+		{Name: Label_type, Value: processor},
+		{Name: Label_errcode, Value: strconv.Itoa(errCode)},
+	}
+}
+func buildSerReqLabelsV2(instance, servkey string, servid int, processor string, source int, funcName string, errCode int, status int) []Label {
+	labels := buildSerLabelsV2(instance, servkey, servid, processor, source, funcName, errCode)
 	labels = append(labels, Label{
 		Name: Label_status, Value: strconv.Itoa(status),
 	})
